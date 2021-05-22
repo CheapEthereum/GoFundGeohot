c3fde9c71220c3d9dfd92a36f1d2fc16c44704c0
Michele Saba <pymike00@gmail.com>
2021-05-22 22:08:37 +0200

    UPDATE: donate.py, just testing

--- scripts/donate.py
+++ scripts/donate.py
@@ -9,7 +9,7 @@ web3 = Web3(HTTPProvider(url))
 web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 # use your own private key
-private_key_hex = "*****your_private_key*****"
+private_key_hex = "XXX"
 
 # setting account checking balance
 my_account = web3.eth.account.from_key(private_key_hex)
@@ -24,16 +24,12 @@ abi = requests.get(abi_url).text
 donate_contract = web3.eth.contract(abi=abi, address=web3.toChecksumAddress('0x891F4cdA9738E0E77D5a12cd209EdB9cbFae30c7'))
 print('Beneficiary address: {}'.format(donate_contract.functions.getBeneficiary().call()))
 
-donate_amount = Web3.toWei(1, 'ether')
+amount_to_get_back = Web3.toWei(0.1, 'ether')  # do we need the amount of one of the transactions we want to get back?
 
 # preparing tx
-donate_transaction = donate_contract.functions.donate()\
+donate_transaction = donate_contract.functions.unDonate()\
 .buildTransaction({
-    'from': self_address,
-    'gasPrice':  Web3.toWei(1, 'gwei'),
-    'gas': 90000,
-    'value': donate_amount,
-    'nonce': web3.eth.getTransactionCount(self_address),
+    'amount': amount_to_get_back,
 })
 signed_donate_transaction = web3.eth.account.signTransaction(donate_transaction, private_key_hex)
 raw_tx_hex = Web3.toHex(signed_donate_transaction.rawTransaction)
@@ -41,3 +37,13 @@ raw_tx_hex = Web3.toHex(signed_donate_transaction.rawTransaction)
 # sending tx
 tx_hash = web3.eth.sendRawTransaction(raw_tx_hex)
 print(tx_hash.hex())
+
+
+"""
+This returns the following error code:
+
+web3.exceptions.ValidationError: 
+Could not identify the intended function with name `unDonate`, positional argument(s) of type `()` and keyword argument(s) of type `{}`.
+Found 1 function(s) with the name `unDonate`: ['unDonate(uint256)']
+Function invocation failed due to improper number of arguments.
+"""
